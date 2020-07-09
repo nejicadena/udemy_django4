05388b7d4bb8368f9cb06b32f656ad0963c52310
nejicadena <nejicadena@gmail.com>
2020-07-09 00:48:04 -0500

    add pages

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,14 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
 
-def sample(request):
-    return render(request, "core/sample.html")
+    template_name = "core/home.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':'Mi super web'})   
+    
+
+class SamplePageView(TemplateView):
+
+    template_name = "core/sample.html"
